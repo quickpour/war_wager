42a1e40c32b0174643f58682f5d41151aa0978eb
Hector Garcia <quickpour@hotmail.com>
2022-01-30 04:14:16 -0800

    upcoming C and fetch all by game

--- flask_app/models/upcoming_event.py
+++ flask_app/models/upcoming_event.py
@@ -1,8 +1,9 @@
+from asyncio import events
 from flask import session, flash
-
+from flask_app.models import user
 from flask_app.config.mysqlconnection import connectToMySQL
 
-class upcoming_event:
+class Upcoming_event:
     def __init__(self, data):
         self.id = data['id']
         self.game = data['game']
@@ -40,3 +41,34 @@ class upcoming_event:
     def create_upcoming_event(cls, data):
         query = "INSERT INTO upcoming_events (game, name, date, time, description, created_at, updated_at, user_id) VALUES (%(game)s, %(name)s, %(date)s, %(time)s, %(description)s, NOW(), NOW(), %(user_id)s);"
         return connectToMySQL(cls.db).query_db(query, data)
+
+    @classmethod
+    def get_upcoming_by_game(cls, data):
+        query = "SELECT * FROM upcoming_events LEFT JOIN users ON upcoming_events.user_id = users.id WHERE game = %(game)s;"
+        results = connectToMySQL(cls.db).query_db(query, data)
+        upcoming_events = []
+        for event in results:
+            event_data = {
+                'id' : event['id'],
+                'game' : event['game'],
+                'name' : event['name'],
+                'date' : event['date'],
+                'time' : event['time'],
+                'description' : event['description'],
+                'created_at' : event['created_at'],
+                'updated_at' : event['updated_at']
+            }
+            single_event = cls(event_data)
+            user_data = {
+                'id' : event['users.id'],
+                'first_name' : event['first_name'],
+                'last_name' : event['last_name'],
+                'email' : event['email'],
+                'password' : event['password'],
+                'created_at' : event['users.created_at'],
+                'updated_at' : event['users.updated_at']
+            }
+            single_user = user.User(user_data)
+            single_event.creator = single_user
+            upcoming_events.append(single_event)
+        return upcoming_events
