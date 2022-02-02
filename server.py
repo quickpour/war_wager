8453597a67cb9b8d277acfd24530d7541ca4de9a
zachjbacon <zacharyjamesbacon@gmail.com>
2022-02-01 21:59:09 -0600

    Added test Controller and Template to display past_events in the future.

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import users, upcoming_events, upcoming_event_comments
+from flask_app.controllers import users, upcoming_events, upcoming_event_comments, past_events
 
 
 if __name__=="__main__":
